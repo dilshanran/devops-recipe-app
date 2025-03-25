673c86af8620c734eb01b08fd92748ca033fe36a
dilshanran <drmanawadu071@gmail.com>
2025-03-25 12:49:51 +0530

    infrasetup

--- infra/setup/output.tf
+++ infra/setup/output.tf
@@ -1,10 +1,10 @@
 output "cd_user_access_key_id" {
- description = "AWS Key ID for CD user"
-  value = aws_iam_access_key.cd.id
+  description = "AWS Key ID for CD user"
+  value       = aws_iam_access_key.cd.id
 }
 
 output "cd_user_access_key_secret" {
   description = "AWS Key Secret for CD user"
-  value = aws_iam_access_key.cd.secret
-  sensitive = true
+  value       = aws_iam_access_key.cd.secret
+  sensitive   = true
 }

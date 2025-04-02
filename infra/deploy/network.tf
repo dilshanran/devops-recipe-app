cb8564b11a244d0865cc8869d77131e6f42d6b35
dilshanran <drmanawadu071@gmail.com>
2025-04-02 09:37:52 +0530

    networkfor subnet

--- infra/deploy/network.tf
+++ infra/deploy/network.tf
@@ -16,3 +16,73 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
+#public subnet for the ALB #
+
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.1.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "${data.aws_region.current.name}a" # this come from variable.tf and main.tf #
+
+  tags = {
+    Name = "${local.prefix}-public-a" # this come from variable.tf and main.tf #
+  }
+}
+
+resource "aws_route_table" "public_a" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${local.prefix}-public-a" # this come from variable.tf and main.tf #
+  }
+
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public_a.id
+
+}
+
+resource "aws_route" "public_internet_access_a" {
+  route_table_id         = aws_route_table.public_a.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+
+}
+
+#public subnet 2 for the ALB #
+
+
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.1.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "${data.aws_region.current.name}b" # this come from variable.tf and main.tf #
+
+  tags = {
+    Name = "${local.prefix}-public-b" # this come from variable.tf and main.tf #
+  }
+}
+
+resource "aws_route_table" "public_b" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${local.prefix}-public-b" # this come from variable.tf and main.tf #
+  }
+
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public_b.id
+
+}
+
+resource "aws_route" "public_internet_access_b" {
+  route_table_id         = aws_route_table.public_b.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+
+}

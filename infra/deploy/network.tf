6e4ca2046ad94c0962b97330ba895c01dd5cb090
dilshanran <drmanawadu071@gmail.com>
2025-04-02 09:19:04 +0530

    adding new vpc

---
+++ infra/deploy/network.tf
@@ -0,0 +1,18 @@
+#Network Infrastucture#
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.1.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
+
+#internet gateway needed for inbound traffic to the ALB #
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "local.prefix}-main" # this come from variable.tf and main.tf #
+  }
+}
+

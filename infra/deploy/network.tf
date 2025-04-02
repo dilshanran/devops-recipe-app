597c434e9c6471cf8bd6ac569142322b69d6ce3d
dilshanran <drmanawadu071@gmail.com>
2025-04-02 09:45:02 +0530

    private subnet

--- infra/deploy/network.tf
+++ infra/deploy/network.tf
@@ -86,3 +86,25 @@ resource "aws_route" "public_internet_access_b" {
   gateway_id             = aws_internet_gateway.main.id
 
 }
+
+#private subnet for the internal access only #
+
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.1.10.0/24"
+  availability_zone = "${data.aws_region.current.name}a" # this come from variable.tf and main.tf #
+
+  tags = {
+    Name = "${local.prefix}-private-a" # this come from variable.tf and main.tf #
+  }
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.1.11.0/24"
+  availability_zone = "${data.aws_region.current.name}b" # this come from variable.tf and main.tf #
+
+  tags = {
+    Name = "${local.prefix}-private-b" # this come from variable.tf and main.tf #
+  }
+}

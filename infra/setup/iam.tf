b0ddfaa23a1b8f0fe282a33a01460c6cb28dc738
dilshanran <drmanawadu071@gmail.com>
2025-04-22 12:33:51 +0530

    testcode13

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -167,7 +167,7 @@ data "aws_iam_policy_document" "rds" {
       "rds:DeleteDBInstance",
       "rds:ModifyDBInstance",
       "rds:ListTagsForResource",
-      "rds:AddTagsToResource"      
+      "rds:AddTagsToResource"
     ]
     resources = ["*"]
   }

a35c0477fc424be96e3fc89564014080eb4466c0
dilshanran <drmanawadu071@gmail.com>
2025-04-22 22:25:14 +0530

    editdb

--- infra/deploy/database.tf
+++ infra/deploy/database.tf
@@ -41,6 +41,7 @@ resource "aws_db_instance" "main" {
   username                   = var.db_username
   password                   = var.db_password
   db_subnet_group_name       = aws_db_subnet_group.main.name
+  skip_final_snapshot        = true
   multi_az                   = false
   backup_retention_period    = 0
   vpc_security_group_ids     = [aws_security_group.rds.id]
